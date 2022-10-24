4d2e5401a659ab406be3c0f14b58415032c6ee54
Bereket Adamsseged <bereketadamsseged@gmail.com>
2022-10-24 22:16:51 +0300

    Mandatory task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Contains the lookup function
+"""
+
+
+def lookup(obj):
+    """returns a list of available attributes and methods of an object"""
+    return dir(obj)
+#!/usr/bin/python3
+"""
+Contains the lookup function
+"""
+
+
+def lookup(obj):
+    """returns a list of available attributes and methods of an object"""
+    return dir(obj)
